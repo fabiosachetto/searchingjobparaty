727ee180ac3f6374a82ba9406d6dba5ad303813a
Fabio Galdo <fabiogaldo@gmail.com>
2024-10-19 18:13:24 -0300

    Co-authored-by: Fábio Verdi Sachetto <fabio@sachetto.com.br>

--- searchingjob/src/app/page.tsx
+++ searchingjob/src/app/page.tsx
@@ -4,14 +4,26 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        {/* Este é o componente de imagem do logotipo do Next.js.
+            Estamos usando o componente "Image" do Next.js para melhor otimização.
+            - className: aplica um estilo que inverte as cores no modo escuro.
+            - src: a URL da imagem.
+            - alt: texto alternativo para acessibilidade.
+            - width: largura da imagem.
+            - height: altura da imagem.
+            - priority: informa ao Next.js para priorizar o carregamento desta imagem.
+        */}
+        {
+          <Image
+            className="dark:invert"
+            src="https://nextjs.org/icons/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+        }
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
