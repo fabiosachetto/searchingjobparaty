8b7dd8a54a860197b67151f604708b7c7a6fe12c
fabiosachetto <fabio@sachetto.com.br>
2024-10-21 20:46:21 -0300

    add icones

---
+++ searchingjob/src/components/Icons/icons/HomeIcon.tsx
@@ -0,0 +1,8 @@
+// https://www.iconfinder.com/icons/8665531/house_icon
+export const HomeIcon = ({ color="currentColor" , className="" }) => {
+    return (
+        <svg className={className} height="32" id="home" viewBox="0 0 32 32" width="32" xmlns="http://www.w3.org/2000/svg">
+            <path fill={color} d="M16 0 L32 16 L28 16 L28 30 L20 30 L20 20 L12 20 L12 30 L4 30 L4 16 L0 16 Z"/>
+        </svg>
+    );
+};
--- searchingjob/src/components/Navbar/Navbar.tsx
+++ searchingjob/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
+import { HomeIcon , LoginIcon } from "@/components";
+
 export const Navbar = () => {
     return (
-        <nav className="flex flex-col gap-3 bg-fuchsia-900 border border-fuchsia-300/80 hover:border-fuchsia-300/40 w-64 p-3">
+        <nav className="flex h-screen flex-col gap-3 bg-fuchsia-900 border border-fuchsia-300/80 hover:border-fuchsia-300/40 w-64 p-3">
           
           <div className="">
             <h1>
@@ -12,15 +14,19 @@ export const Navbar = () => {
             </h1>
           </div>
 
-          <ul className="my-3 border-t border-fuchsia-300 hover:">
-            <li className="">Home</li>
-            <li>Login</li>
-            <li>Fale Conosco</li>
-            <li>Termos e Condições</li>
+          <ul className="flex-grow my-4 border-t border-fuchsia-300">
+            <li className="my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700 hover:text-slate-950 flex gap-2 items-center">
+              <HomeIcon className="w-4 h-4"/> Home
+            </li>
+            <li className="my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700 hover:text-slate-950 flex gap-2 items-center">
+              <LoginIcon className="w-4 h-4"/> Login
+            </li>
+            <li className="my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700 hover:text-slate-950 flex gap-2 items-center">Fale Conosco</li>
+            <li className="my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700 hover:text-slate-950 flex gap-2 items-center">Termos e Condições</li>
           </ul>
 
-          <ul className="my-3 border-t border-fuchsia-300 hover:">
-            <li>Usuário</li>
+          <ul className="my-4 border-t border-fuchsia-300">
+            <li className="my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700  hover:text-slate-950 flex gap-2 items-center">Usuário</li>
           </ul>
 
         </nav>
