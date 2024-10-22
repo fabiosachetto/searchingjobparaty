01ae6872f78b14b4815eaf2ee39a341015e42fc6
fabiosachetto <fabio@sachetto.com.br>
2024-10-22 17:05:11 -0300

    melhorando a forma de utilizar o codigo para ser reaproveitavel

--- searchingjob/src/components/Navbar/Navbar.tsx
+++ searchingjob/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,15 @@
 import { HomeIcon , LoginIcon , FaleConoscoIcon , UsuarioIcon } from "@/components";
 
+const NavbarUl = ( { children , className} ) => {
+  return <ul className={`my-4 border-t border-fuchsia-300 ${className}`}>{ children }</ul>;
+};
+
+const NavbarUlLi = ( { children , className} ) => {
+  return (<li className={`my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700 hover:text-slate-950 flex gap-2 items-center ${className}`}>
+    { children }
+  </li>);
+};
+
 export const Navbar = () => {
     return (
         <nav className="flex h-screen flex-col bg-fuchsia-900 border border-fuchsia-300 hover:border-fuchsia-700 w-64 p-3">
@@ -14,25 +24,23 @@ export const Navbar = () => {
             </h1>
           </div>
 
-          <ul className="flex-grow my-4 border-t border-fuchsia-300">
-            <li className="my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700 hover:text-slate-950 flex gap-2 items-center">
+          <NavbarUl className={"flex-grow"}>
+            <NavbarUlLi>
               <HomeIcon className="w-4 h-4"/> Home
-            </li>
-            <li className="my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700 hover:text-slate-950 flex gap-2 items-center">
+            </NavbarUlLi>
+            <NavbarUlLi>
               <LoginIcon className="w-4 h-4"/> Login
-            </li>
-            <li className="my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700 hover:text-slate-950 flex gap-2 items-center">
+            </NavbarUlLi>
+            <NavbarUlLi>
               <FaleConoscoIcon className="w-4 h-4"/> Fale Conosco
-            </li>
-            {/* <li className="my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700 hover:text-slate-950 flex gap-2 items-center">Termos e Condições</li> */}
-          </ul>
+            </NavbarUlLi>
+          </NavbarUl>
 
-          <ul className="my-4 border-t border-fuchsia-300">
-            <li className="my-2 rounded-lg p-2 cursor-pointer bg-transparent hover:bg-fuchsia-700 hover:text-slate-950 flex gap-2 items-center">
+          <NavbarUl>
+            <NavbarUlLi>
               <UsuarioIcon className="w-5 h-5"/> Usuário
-            </li>
-          </ul>
-
+            </NavbarUlLi>
+          </NavbarUl>
         </nav>
     );
 }
