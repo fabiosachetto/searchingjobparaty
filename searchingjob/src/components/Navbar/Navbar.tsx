3d7d030066059b745119d84cb6d59b13bfe239b3
fabiosachetto <fabio@sachetto.com.br>
2024-10-24 16:30:06 -0300

    alterando o caminho do logo

--- searchingjob/src/components/Navbar/Navbar.tsx
+++ searchingjob/src/components/Navbar/Navbar.tsx
@@ -19,11 +19,11 @@ export const Navbar = ({className , ...props}: NavbarProps) => {
           <div className="flex items-center justify-center">
             <h1>
               <Image 
-                src="https://github.com/fabiosachetto/searchingjobparaty/blob/navbar/searchingjob/src/app/img/logo.png"
+                src="https://github.com/fabiosachetto/searchingjobparaty/blob/navbar/searchingjob/src/app/img/logo.png?raw=true"
                 alt="Searching Job Paraty"
                 className="w-auto h-40 p-2"
-                width={214}
-                height={144}
+                width={256}
+                height={256}
               />
             </h1>
           </div>
